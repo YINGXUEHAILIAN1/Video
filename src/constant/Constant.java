ef1ac0785b422562a061099e8999fe9eb773bb74
Uther <dandi007@qq.com>
2016-04-09 18:19:10 +0800

    添加常亮类

---
+++ src/constant/Constant.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Uther on 2016/4/9.
+ */
+
+package constant;
+
+import java.util.Set;
+
+public class Constant {
+    public static final String NORMAL = "NORMAL";
+    public static final String ARTIST = "ARTIST";
+    public static final String MALE = "MALE";
+    public static final String FEMALE = "FEMALE";
+    public static final String ORIGIN = "ORIGIN";
+    public static final String SHARE = "SHARE";
+    public static final String WAIT = "WAIT";
+    public static final String PASS = "PASS";
+    public static final String PUBLIC = "TRUE";
+    public static final String NOT_PUBLIC = "FALSE";
+}
