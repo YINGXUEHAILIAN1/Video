a5d5caedf867b675aa1bee7543b4ae1584e73bce
Uther <dandi007@qq.com>
2016-04-09 18:41:36 +0800

    完成第一阶段debug User和Video的关联测试没问题，现在给Video加入upLoadUser

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import constant.Constant;
 import entity.UserEntity;
+import entity.VideoEntity;
 
 import java.sql.Date;
 
@@ -9,6 +10,21 @@ import java.sql.Date;
  * Created by Uther on 2016/4/9.
  */
 public class UserDAO extends Dao {
+
+    public UserEntity getUser(int id){
+        UserEntity user = null;
+        try{
+            begin();
+            user = (UserEntity)getSession().get(UserEntity.class,id);
+            commit();
+        }catch (Exception e){
+            rollback();
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
 
     public static void main(String[] args){
         UserEntity user = new UserEntity();
@@ -20,6 +36,25 @@ public class UserDAO extends Dao {
         user.setSex(Constant.MALE);
         user.setUserType(Constant.ARTIST);
         user.setRegisterDate(new java.sql.Date(System.currentTimeMillis()));
-        new UserDAO().save(user);
+        UserDAO dao = new UserDAO();
+        dao.save(user);
+
+        VideoEntity video = new VideoEntity();
+        video.setFile(new byte[10]);
+        video.setFirstType(Constant.ORIGIN);
+        video.setSecondType("旅游");
+        video.setIsPublic(Constant.NOT_PUBLIC);
+        video.setStatus(Constant.WAIT);
+        video.setUploadDate(new java.sql.Date(System.currentTimeMillis()));
+        video.setVideoName("f");
+        dao.save(video);
+        user.getCollections().add(video);
+        dao.update(user);
+
+        UserEntity nu = dao.getUser(user.getUserId());
+        for (VideoEntity v : nu.getCollections()){
+            System.out.println(v.getVideoName());
+        }
+
     }
 }
